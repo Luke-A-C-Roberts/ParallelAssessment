23b63939e4fff6a6e1b06e37cec1a3995390a5a1
Computing <Computing@R5-WINDOWS>
2024-03-13 19:07:12 +0000

    fixed HistFilter

--- AssessmentProj/main.cpp
+++ AssessmentProj/main.cpp
@@ -119,60 +119,52 @@ auto HistFilter<T>::_make_io_buffers(const cl::Context& context, const UINT_PTR&
 template<typename T>
 void HistFilter<T>::output() {
 	//detect any potential exceptions
-	try {
-		// this would look better with c++17 structured bindings but alas
-		// Image
-		auto input = _load_image<T>(_image_filename);
-		auto& image_input = input.first;
-		auto& disp_input = input.second;
-		auto size_input = image_input.size();
+	// this would look better with c++17 structured bindings but alas
+	// Image
+	auto input = _load_image(_image_filename);
+	auto& image_input = input.first;
+	auto& disp_input = input.second;
+	auto size_input = image_input.size();
 
-		/*
-		A cl::Context is used so that opencl can manage memory, devives and error handling.
-		Opencl errors are passed back to the program and handeled by catch in with a cl::Error.
-		Then a cl::CommandQueue is created so that opencl commands can be queued and ran asynchronously.
-		A cl::ProgramSources class is used to retrieve the opencl source code from kernels.cl and then
-		the program is constructed using both our context and sources. Finally `build_kernel` (see above)
-		is called to build `program` and handel debuging output and exceptions.
-		*/
-		auto context = GetContext(_platform_id, _device_id);
-		cl::CommandQueue queue(context);
-		cl::Program::Sources sources;
-		AddSources(sources, _kernel_filename);
-		cl::Program program(context, sources);
-		_build_kernel(program, context, _debug);
+	/*
+	A cl::Context is used so that opencl can manage memory, devives and error handling.
+	Opencl errors are passed back to the program and handeled by catch in with a cl::Error.
+	Then a cl::CommandQueue is created so that opencl commands can be queued and ran asynchronously.
+	A cl::ProgramSources class is used to retrieve the opencl source code from kernels.cl and then
+	the program is constructed using both our context and sources. Finally `build_kernel` (see above)
+	is called to build `program` and handel debuging output and exceptions.
+	*/
+	auto context = GetContext(_platform_id, _device_id);
+	cl::CommandQueue queue(context);
+	cl::Program::Sources sources;
+	AddSources(sources, _kernel_filename);
+	cl::Program program(context, sources);
+	_build_kernel(program, context, _debug);
 
-		// cl::Buffer created to hold image data
-		auto io_buffers = _make_io_buffers<T>(context, size_input);
-		auto& dev_image_input = io_buffers.first;
-		auto& dev_image_output = io_buffers.second;
+	// cl::Buffer created to hold image data
+	auto io_buffers = _make_io_buffers(context, size_input);
+	auto& dev_image_input = io_buffers.first;
+	auto& dev_image_output = io_buffers.second;
 
-		queue.enqueueWriteBuffer(dev_image_input, CL_TRUE, 0, image_input.size(), &image_input.data()[0]);
+	queue.enqueueWriteBuffer(dev_image_input, CL_TRUE, 0, image_input.size(), &image_input.data()[0]);
 
-		cl::Kernel kernel = cl::Kernel(program, "identity");
-		kernel.setArg(0, dev_image_input);
-		kernel.setArg(1, dev_image_output);
+	cl::Kernel kernel = cl::Kernel(program, "identity");
+	kernel.setArg(0, dev_image_input);
+	kernel.setArg(1, dev_image_output);
 
-		queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(image_input.size()), cl::NullRange);
+	queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(image_input.size()), cl::NullRange);
 
-		vector<T> output_buffer(image_input.size());
-		queue.enqueueReadBuffer(dev_image_output, CL_TRUE, 0, output_buffer.size(), &output_buffer.data()[0]);
+	vector<T> output_buffer(image_input.size());
+	queue.enqueueReadBuffer(dev_image_output, CL_TRUE, 0, output_buffer.size(), &output_buffer.data()[0]);
 
-		CImg<T> output_image(output_buffer.data(), image_input.width(), image_input.height(), image_input.depth(), image_input.spectrum());
-		CImgDisplay disp_output(output_image, "output");
+	CImg<T> output_image(output_buffer.data(), image_input.width(), image_input.height(), image_input.depth(), image_input.spectrum());
+	CImgDisplay disp_output(output_image, "output");
 		
-		while (!disp_input.is_closed() && !disp_output.is_closed() && !disp_input.is_keyESC() && !disp_output.is_keyESC()) {
-			disp_input.wait(1);
-			disp_output.wait(1);
-		}
+	while (!disp_input.is_closed() && !disp_output.is_closed() && !disp_input.is_keyESC() && !disp_output.is_keyESC()) {
+		disp_input.wait(1);
+		disp_output.wait(1);
+	}
 
-	}
-	catch (const cl::Error& err) {
-		std::cerr << "ERROR: " << err.what() << ", " << getErrorString(err.err()) << std::endl;
-	}
-	catch (const CImgException& err) {
-		std::cerr << "ERROR: " << err.what() << std::endl;
-	}
 }
 
 auto main(i32 argc, str* argv) -> i32 {
@@ -188,6 +180,17 @@ auto main(i32 argc, str* argv) -> i32 {
 	handle_args(argc, argv, platform_id, device_id, debug);
 
 	cimg::exception_mode(0);
+
+	try {
+		HistFilter<u8> hist_filter(image_filename, kernel_filename, platform_id, device_id, debug);
+		hist_filter.output();
+	}
+	catch (const cl::Error& err) {
+		std::cerr << "ERROR: " << err.what() << ", " << getErrorString(err.err()) << std::endl;
+	}
+	catch (const CImgException& err) {
+		std::cerr << "ERROR: " << err.what() << std::endl;
+	}
 
 	return EXIT_SUCCESS;
 }
